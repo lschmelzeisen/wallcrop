419bef848e5057d0b126de6da109217f5e1ee6f2
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2021-02-04 23:51:14 +0100

    Project boilerplate.

---
+++ README.rst
@@ -0,0 +1,13 @@
+========================================================================================
+wallcrop
+========================================================================================
+
+Multi-monitor wallpaper cropping tool.
+
+License
+========================================================================================
+
+Copyright 2021 Lukas Schmelzeisen.
+Licensed under the
+`Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>`_.
+
---
+++ src/wallcrop/_cli.py
@@ -0,0 +1,49 @@
+#
+# Copyright 2021 Lukas Schmelzeisen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from pathlib import Path
+from sys import stdout
+
+from nasty_utils import (
+    Argument,
+    LoggingSettings,
+    Program,
+    ProgramConfig,
+    SettingsConfig,
+)
+from overrides import overrides
+
+import wallcrop
+
+
+class WallcropSettings(LoggingSettings):
+    class Config(SettingsConfig):
+        search_path = Path("wallcrop.toml")
+
+
+class WallcropProgram(Program):
+    class Config(ProgramConfig):
+        title = "wallcrop"
+        version = wallcrop.__version__
+        description = "Multi-monitor wallpaper cropping tool."
+
+    settings: WallcropSettings = Argument(
+        alias="config", description="Overwrite default config file path."
+    )
+
+    @overrides
+    def run(self) -> None:
+        stdout.write("To-do\n")
